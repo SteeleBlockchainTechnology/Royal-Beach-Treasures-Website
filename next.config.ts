4ab3f6d5fb2e0c695d942f19a829aa272750c5fd
sturgis <sturgis.steele@outlook.com>
2025-05-15 01:00:21 -0500

    githuhb pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type {NextConfig} from 'next';
 
+const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : 'Royal-Beach-Treasures-Website';
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
@@ -9,7 +12,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : ''
 };
 
 export default nextConfig;
