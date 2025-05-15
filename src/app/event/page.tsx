a986159db3abb612e92c59141bc7cf9672637e37
sturgis <sturgis.steele@outlook.com>
2025-05-15 11:31:36 -0500

    add correct image sources

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -15,7 +15,7 @@ export default function EventPage() {
          <div className="relative aspect-video rounded-lg overflow-hidden shadow-lg">
             {/* Placeholder for map or event image */}
              <Image
-                src="/images/city.jpg"
+                src="./images/city.jpg"
                 alt="Map or image of Corpus Christi Kids Market location"
                 layout="fill"
                 objectFit="cover"
